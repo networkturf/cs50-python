29af435f7d4f8de34bb62f79de2de5b0c175a697
Kunal Vaidya <networkturf@gmail.com>
2023-06-20 09:40:19 -0700

    Added strings tutorial

---
+++ strings.py
@@ -0,0 +1,15 @@
+name = "Kunal Vaidya"
+
+#To replace text in strings in a print statement:
+print(f"My name is: {name}")
+      
+#To remove whitespace from a string:
+name = name.strip()
+
+#To capitalize first letter: 
+name = name.title()
+
+#To split the name into 2 strings: 
+first, last = name.split(" ")
+
+print(f"My first name is {first} and last name is {last} ")
